08c4082f900de1a86d4a5c47764970156c99bc16
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2025-10-30 15:26:47 -0300

    Adição da busca por id

--- API_agrofamilia_pesca/Utils/dbUtils.js
+++ API_agrofamilia_pesca/Utils/dbUtils.js
@@ -8,4 +8,18 @@ async function views(view) {
     return await knex.select("").table(view);
 };
 
-module.exports = { findAll, views };
+async function findBy(field, value, multiple = false, table) {
+    const result = await knex.select("").from(table).where({ [field]: value });
+
+    if (result.length) {
+        if (multiple) {
+            return result;
+        }else{
+            return result[0];
+        };
+    }else{
+        return undefined;
+    };
+};
+
+module.exports = { findAll, views, findBy };
