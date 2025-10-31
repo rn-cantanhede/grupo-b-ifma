de8cedd57a544dafd68b2e57b7e46d3e12174b83
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2025-10-31 00:04:57 -0300

    Adição do get para produtos

---
+++ API_agrofamilia_pesca/controllers/ProdutosController.js
@@ -0,0 +1,15 @@
+const Produtos = require("../model/Produtos");
+
+class ProdutosController {
+    async findAllProdutos(req, res){
+        try {
+            const produtos = await Produtos.findAllProdutos();
+            res.status(200).json(produtos);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ Error: "Erro interno no servidor" });
+        }
+    };
+};
+
+module.exports = new ProdutosController();
