8155e52a612751296ec66e354e37b34ac1f787ef
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2025-11-18 15:31:07 -0300

    feat(api): Configurção do index com body-parser para aceita requisições para post
    feat(api) Adição do insert no database

--- API_agrofamilia_pesca/Utils/verifyUtils.js
+++ API_agrofamilia_pesca/Utils/verifyUtils.js
@@ -4,7 +4,7 @@ class Verify {
             return res.status(404).json({ Error: "Este id não existe" });
         };
     };
-    Exists(res, value){
+    Exists(res, value) {
         if (value) {
             return res.status(403).json({ Error: "Este id já existe" });
         };
