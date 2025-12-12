8598c590537956f694fc4bc10f2253362343e256
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2025-12-11 22:33:55 -0300

    feat(api): Adição do PUT para modificar uma categoria

--- API_agrofamilia_pesca/modules/Categorias/categorias.routes.js
+++ API_agrofamilia_pesca/modules/Categorias/categorias.routes.js
@@ -7,4 +7,6 @@ router.get("/:value", CategoriasController.findCategoria);
 
 router.post("/new", CategoriasController.createCategoria);
 
+router.put("/update/:id", CategoriasController.updateCategoria);
+
 module.exports = router;
