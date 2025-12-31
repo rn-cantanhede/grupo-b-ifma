c5ccd82dfbba361cb3c55133b7592f6457449ea8
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2025-12-31 00:09:10 -0300

    Adição de comentario

--- API_agrofamilia_pesca/middleware/Authorize.js
+++ API_agrofamilia_pesca/middleware/Authorize.js
@@ -1,5 +1,13 @@
 const Erros = require("../shared/errors/Errors");
 
+/**
+ * Middleware de autorização baseado em nível de acesso.
+ *
+ * Recebe como parâmetro o nível mínimo exigido para acessar
+ * determinado recurso e retorna uma função middleware do Express.
+ *
+ * Quanto menor o número do nível, maior o privilégio.
+ */
 module.exports = function authorize(nivel) {
     return (req, res, next) => {
         if (!req.user || typeof req.user.nivel !== "number") {
