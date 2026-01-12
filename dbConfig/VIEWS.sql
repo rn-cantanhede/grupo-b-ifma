68cd0962b0e571e9501f7eb4ae992dc016fb056f
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-01-12 00:59:31 -0300

    feat(db): Adição de secretaria na view_pessoas

--- dbConfig/VIEWS.sql
+++ dbConfig/VIEWS.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE VIEW view_pessoas AS
-SELECT p.ID, p.NOME, p.CPF, a.CAF, a.VALIDADE_CAF, ac.NOME AS ASSOCIACAO, af.DAP
+SELECT p.ID, p.NOME, p.CPF, a.CAF, a.VALIDADE_CAF, af.DAP, 
+ac.NOME AS ASSOCIACAO, sec.NOME AS SECRETARIA
 FROM pessoa p 
 JOIN associado a ON a.ID_PESSOA = p.ID
 JOIN associacao ac ON ac.ID = a.ID_ASSOCIACAO
-JOIN agricultura_familiar af ON af.ID_ASSOCIADO = a.ID;
+JOIN agricultura_familiar af ON af.ID_ASSOCIADO = a.ID
+JOIN secretaria sec ON sec.ID = ac.ID_SECRETARIA;
 
 CREATE OR REPLACE VIEW view_associacoes AS
 SELECT ac.ID, ac.NOME, cat.NOME AS CATEGORIA, sec.NOME AS SECRETARIA
@@ -12,7 +14,8 @@ JOIN categoria cat ON cat.ID = ac.ID_CATEGORIA
 JOIN secretaria sec ON sec.ID = ac.ID_SECRETARIA;
 
 CREATE OR REPLACE VIEW view_localizacao_beneficiado AS
-SELECT loc.ID, ps.NOME, assoc.NOME AS ASSOCIACAO, loc.LATITUDE, loc.LONGITUDE, loc.TITULO, loc.DESCRICAO
+SELECT loc.ID, ps.NOME, assoc.NOME AS ASSOCIACAO, loc.LATITUDE, 
+loc.LONGITUDE, loc.TITULO, loc.DESCRICAO
 FROM localizacao_beneficiada loc
 JOIN associado asso ON asso.ID_PESSOA = loc.ID_ASSOCIADO
 JOIN pessoa ps ON ps.ID = asso.ID_PESSOA
@@ -24,8 +27,8 @@ FROM produto pro
 JOIN tipo_produto tipro ON tipro.ID = pro.ID_TIPO_PRODUTO;
 
 CREATE OR REPLACE VIEW view_produto_movimentacao AS
-SELECT movi.ID, agri.DAP, pro.NOME AS PRODUTO, movi.QNT_PRODUZIDA, movi.VLR_UNITARIO, 
-movi.DATA_MOVIMENTACAO, loca.LATITUDE, loca.LONGITUDE
+SELECT movi.ID, agri.DAP, pro.NOME AS PRODUTO, movi.QNT_PRODUZIDA, 
+movi.VLR_UNITARIO, movi.DATA_MOVIMENTACAO, loca.LATITUDE, loca.LONGITUDE
 FROM produto_movimentacao movi
 JOIN agricultura_familiar agri ON agri.ID = movi.ID_AGRICULTURA_FAMILIAR
 JOIN produto pro ON pro.ID = movi.ID_PRODUTO
@@ -45,7 +48,8 @@ JOIN pessoa pe ON asso.ID_PESSOA = pe.ID
 JOIN programa prog ON prog.ID = agri.ID_PROGRAMA;
 
 CREATE OR REPLACE VIEW view_usuarios AS
-SELECT user.ID, pe.NOME, user.NIVEL, sec.NOME AS SECRETARIA, asso.NOME AS ASSOCIACAO, user.LOGIN
+SELECT user.ID, pe.NOME, user.NIVEL, sec.NOME AS SECRETARIA, 
+asso.NOME AS ASSOCIACAO, user.LOGIN
 FROM usuario AS user
 JOIN pessoa pe ON pe.ID = user.ID_PESSOA
 JOIN secretaria sec ON sec.ID = user.ID_SECRETARIA
