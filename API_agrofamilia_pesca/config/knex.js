855821e9d1382daa1a08397798e7d0c424bb79b5
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-01-15 13:58:19 -0300

    feat(api): Criação do ambiente de desenvolvimento no IFMA(.env.ifma) e configuração no config/knex.js

--- API_agrofamilia_pesca/config/knex.js
+++ API_agrofamilia_pesca/config/knex.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV || "development";
 
 /**
  * Configurações de conexão com o banco de dados
@@ -20,6 +20,21 @@ const configs = {
         pool: {
             min: 2,
             max: 10
+        },
+    },
+
+    development_ifma: {
+        client: "mysql2",
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: "",
+            database: process.env.DB_NAME,
+            timezone: "-03:00",
+        },
+        pool: {
+            min: 2,
+            max: 10,
         },
     },
 };
