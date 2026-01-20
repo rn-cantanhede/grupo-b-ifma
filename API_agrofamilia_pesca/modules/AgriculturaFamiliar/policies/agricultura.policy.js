2670370d53063e336e291da24a920c5fdff3c68b
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-01-20 13:12:52 -0300

    feat(api): Criação das politicas de AgriculturaFamiliar.

---
+++ API_agrofamilia_pesca/modules/AgriculturaFamiliar/policies/agricultura.policy.js
@@ -0,0 +1,41 @@
+const LevelPolicy = require("../../../shared/policies/level.policy");
+
+class AgriculturaPolicy {
+
+    static canGet(user) {
+        return LevelPolicy.hasAccess(user, [
+            LevelPolicy.LEVELS.ADMIN,
+            LevelPolicy.LEVELS.SECRETARIA,
+            LevelPolicy.LEVELS.ASSOCIACAO,
+            LevelPolicy.LEVELS.USUARIO
+        ]);
+    }
+
+    static canPost(user) {
+        return LevelPolicy.hasAccess(user, [
+            LevelPolicy.LEVELS.ADMIN,
+            LevelPolicy.LEVELS.SECRETARIA,
+            LevelPolicy.LEVELS.ASSOCIACAO
+        ]);
+    }
+
+    static canUpdate(user, targetUser) {
+        if (LevelPolicy.isAdmin(user)) return true;
+
+        if (LevelPolicy.isSecretaria(user)) {
+            return user.secretaria == targetUser.ID_SECRETARIA;
+        }
+
+        if (LevelPolicy.isAssociacao(user)) {
+            return user.associacao == targetUser.ID_ASSOCIACAO;
+        }
+
+        return false;
+    }
+
+    static canDelete(user, targetUser) {
+        return this.canUpdate(user, targetUser);
+    }
+}
+
+module.exports = AgriculturaPolicy;
