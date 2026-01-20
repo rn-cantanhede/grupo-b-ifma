4a93d252ce5a7a62d0435374749c3040dd2569de
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-01-19 21:58:30 -0300

    feat(api): Criação das politicas de Programas.

---
+++ API_agrofamilia_pesca/modules/Programas/policies/programas.policy.js
@@ -0,0 +1,36 @@
+const LevelPolicy = require("../../../shared/policies/level.policy");
+
+class ProgramasPolicy {
+
+    static canGet(user) {
+        return LevelPolicy.hasAccess(user, [
+            LevelPolicy.LEVELS.ADMIN,
+            LevelPolicy.LEVELS.SECRETARIA,
+            LevelPolicy.LEVELS.ASSOCIACAO,
+            LevelPolicy.LEVELS.USUARIO
+        ]);
+    };
+
+    static canPost(user) {
+        return LevelPolicy.hasAccess(user, [
+            LevelPolicy.LEVELS.ADMIN,
+            LevelPolicy.LEVELS.SECRETARIA,
+        ]);
+    };
+
+    static canUpdate(user, targetUser) {
+        if (LevelPolicy.isAdmin(user)) return true;
+
+        if (LevelPolicy.isSecretaria(user)) {
+            return user.secretaria == targetUser.ID_SECRETARIA;
+        };
+
+        return false;
+    };
+
+    static canDelete(user, targetUser) {
+        return this.canUpdate(user, targetUser);
+    };
+};
+
+module.exports = ProgramasPolicy;
