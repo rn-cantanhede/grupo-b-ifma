eb31d43f12683fdb308d3d612544037780ca3bc5
rn-cantanhede <146150825+rn-cantanhede@users.noreply.github.com>
2026-06-29 21:32:30 -0300

    feat(api): Migrate da tabela tipo_produto

---
+++ API_agrofamilia_pesca/database/migrations/20260629163203_create_tipo_produto_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("TIPO_PRODUTO", (table) => {
+        table.increments("ID").primary();
+        table.string("NOME", 100).notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable("TIPO_PRODUTO");
+};
