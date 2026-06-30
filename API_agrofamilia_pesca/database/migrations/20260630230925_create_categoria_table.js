76f4beba903df8035ac8e87a88d1345fa4544e32
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-06-30 20:14:43 -0300

    feat(api): Migrate da tabela categoria

---
+++ API_agrofamilia_pesca/database/migrations/20260630230925_create_categoria_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("CATEGORIA", (table) => {
+        table.increments("ID").primary();
+        table.string("NOME", 150).notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable("CATEGORIA");
+};
