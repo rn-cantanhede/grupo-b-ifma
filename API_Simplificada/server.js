913383d3b3f6c06759f93ad3f22344be13d232a5
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-07-02 23:38:09 -0300

    feat(api_simplificada): Automação na criação do database

--- API_Simplificada/server.js
+++ API_Simplificada/server.js
@@ -1,6 +1,31 @@
 const app = require("./app");
+const knex = require("./database/connection");
+const createDB = require("./database/createDB");
+
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Servidor funcionado na porta: ${PORT}`);
-});
+/**
+ * Inicializa o servidor HTTP somente após
+ * validar a conexão com o banco de dados.
+ */
+
+(async function startServer() {
+    try {
+        // Cria o banco caso não exista
+        await createDB();
+
+        // Só depois cria a conexão com o Knex
+        const knex = require("./database/connection");
+        const connectionCheck = require("./database/connectionCheck");
+
+        await connectionCheck(knex);
+
+        app.listen(PORT, () => {
+            console.log(`Servidor funcionando na porta: ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Erro na conexão");
+        console.error(error);
+        process.exit(1);
+    }
+})();
