2b8d3a9a315c201d0484cd39429eef09b4304b25
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-07-21 20:55:39 -0300

    feat(api): Criação do BaseScope para definir os escopo nas rotas de UPDATE

---
+++ API_agrofamilia_pesca/shared/base/baseScope.js
@@ -0,0 +1,28 @@
+const Erros = require("../errors/Errors");
+
+/**
+ * Aplica filtro de escopo para limitar o acesso por nivel de privilegio
+ * 
+ * Subistitudo aprimorado do BaseService, melhorando a performace e a logica
+ */
+class baseScop {
+    async update(id, user, session, fieldSession, fieldUser, method) {
+        if (session.nivel === 1) {
+            return await method(id, user);
+        };
+
+        if (session.nivel === 2) {
+            if (parseInt(user[fieldUser]) !== session[fieldSession]) {
+                throw new Erros("Não autorizado", 403);
+            };
+
+            const { [fieldUser]: removedField, ...filterUser } = user;
+
+            return await method(id, filterUser);
+        } else {
+            throw new Erros("Não autorizado", 403);
+        };
+    };
+};
+
+module.exports = new baseScop();
