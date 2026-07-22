da78fd2e8dffd69b572b9b0ac4d9373c7f9fc21d
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-07-21 23:49:45 -0300

    feat(api): Criação do metodo delete no baseScope para definir os escopo nas rotas de DELETE

--- API_agrofamilia_pesca/shared/base/baseScope.js
+++ API_agrofamilia_pesca/shared/base/baseScope.js
@@ -23,6 +23,22 @@ class baseScop {
             throw new Erros("Não autorizado", 403);
         };
     };
+
+    async delete(id, user, session, fieldSession, fieldUser, method) {
+        if (session.nivel === 1) {
+            return await method(id);
+        };
+
+        if (session.nivel === 2) {
+            if (parseInt(user[fieldUser]) !== session[fieldSession]) {
+                throw new Erros("Não autorizado", 403);
+            };
+
+            return await method(id);
+        } else {
+            throw new Erros("Não autorizado", 403);
+        };
+    };
 };
 
 module.exports = new baseScop();
