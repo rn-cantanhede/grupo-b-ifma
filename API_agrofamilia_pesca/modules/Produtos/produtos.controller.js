191bdbc5e2089475ec22b539a123c332c3edd398
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-08-11 22:55:28 -0300

    refactor(api): Implementação do sistema de log no ProdutosController

--- API_agrofamilia_pesca/modules/Produtos/produtos.controller.js
+++ API_agrofamilia_pesca/modules/Produtos/produtos.controller.js
@@ -13,8 +13,23 @@ class ProdutosController {
     async AllProdutos(req, res) {
         try {
             const produtos = await ProdutosService.findAllProdutos(req.user);
+
+            req.log.info({
+                event: "PRODUTO_LIST",
+                resource: "produto",
+                action: "list",
+                usuarioID: req.user.id
+            }, "Listagem dos produtos");
+
             return res.status(200).json(produtos);
         } catch (error) {
+            req.log.error({
+                event: "PRODUTO_LIST_ERROR",
+                resource: "produto",
+                action: "list",
+                usuarioID: req.user.id
+            }, "Erro ao listar os produtos");
+
             console.error(error);
             return res.status(500).json({ Error: "Erro interno no servidor" });
         };
@@ -30,8 +45,25 @@ class ProdutosController {
                 req.params.value,
                 req.user
             );
+
+            req.log.info({
+                event: "PRODUTO_FIND",
+                resource: "produto",
+                action: "find",
+                usuarioId: req.user.id,
+                target: req.params.value
+            }, "Produto consultado por id ou nome");
+
             return res.status(200).json(produtos);
         } catch (error) {
+            req.log.error({
+                event: "PRODUTO_FIND_ERROR",
+                resource: "produto",
+                action: "find",
+                usuarioID: req.user.id,
+                target: req.params.value
+            }, "Erro ao buscar produto");
+
             console.error(error);
             return next(error);
         };
@@ -47,8 +79,23 @@ class ProdutosController {
                 req.body,
                 req.user
             );
+
+            req.log.info({
+                event: "PRODUTO_CREATE",
+                resource: "produto",
+                action: "create",
+                usuarioId: req.user.id,
+            }, "Produto criado");
+
             return res.status(201).json(result);
         } catch (error) {
+            req.log.error({
+                event: "PRODUTO_CREATE_ERROR",
+                resource: "produto",
+                action: "create",
+                usuarioId: req.user.id
+            }, "Erro ao criar produto");
+
             console.error(error);
             return next(error);
         };
@@ -65,8 +112,25 @@ class ProdutosController {
                 req.body,
                 req.user
             );
+
+            req.log.info({
+                event: "PRODUTO_UPDATE",
+                resource: "produto",
+                action: "update",
+                usuarioId: req.user.id,
+                targetId: req.params.id
+            }, "Produto atualizada");
+
             return res.status(200).json(result);
         } catch (error) {
+            req.log.error({
+                event: "PRODUTO_UPDATE_ERROR",
+                resource: "produto",
+                action: "update",
+                usuarioId: req.user.id,
+                targetId: req.params.id
+            }, "Erro ao atualizar produto");
+
             console.error(error);
             return next(error);
         };
@@ -82,8 +146,25 @@ class ProdutosController {
                 req.params.id,
                 req.user
             );
+
+            req.log.info({
+                event: "PRODUTO_DELETE",
+                resource: "produto",
+                action: "delete",
+                usuarioId: req.user.id,
+                targetId: req.params.id
+            }, "Produto excluído");
+
             return res.status(200).json(result);
         } catch (error) {
+            req.log.error({
+                event: "PRODUTO_DELETE_ERROR",
+                resource: "produto",
+                action: "delete",
+                usuarioId: req.user.id,
+                targetId: req.params.id
+            }, "Erro ao apagar produto");
+
             console.error(error);
             return next(error);
         };
