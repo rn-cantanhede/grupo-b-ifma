72b91ccbbb66a2aabc58870458058b209b0af232
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-08-23 00:11:02 -0300

    refactor(api): Aplicação do middleware pagination nas rotas de admin

--- API_agrofamilia_pesca/modules/Usuarios/routes/usuarios.admin.routes.js
+++ API_agrofamilia_pesca/modules/Usuarios/routes/usuarios.admin.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UsuariosController = require("../usuarios.controller");
+const pagination = require("../../../middleware/pagination");
 
 /**
  * ROTAS APENAS PARA PERMISSÃO NIVEL ADMIN(1);
@@ -11,19 +12,19 @@ const UsuariosController = require("../usuarios.controller");
  */
 
 // Busca usuario pela secretaria.
-router.get("/secretaria/:secretaria", UsuariosController.findSecretariaUsuarios);
+router.get("/secretaria/:secretaria", pagination, UsuariosController.findSecretariaUsuarios);
 
 // Busca usuario pelo nivel.
-router.get("/nivel/:nivel", UsuariosController.findNivelUsuarios);
+router.get("/nivel/:nivel", pagination, UsuariosController.findNivelUsuarios);
 
 // Busca usuario pelo login.
-router.get("/login/:login", UsuariosController.findByLogin);
+router.get("/login/:login", pagination, UsuariosController.findByLogin);
 
 // Retorna todos os usuarios cadastrados.
-router.get("/", UsuariosController.findAllUsuarios);
+router.get("/", pagination, UsuariosController.findAllUsuarios);
 
 // Busca um usuario por um valor genérico.
-router.get("/:value", UsuariosController.findUsuarios);
+router.get("/:value", pagination, UsuariosController.findUsuarios);
 
 /**
  * ROTAS DE CRIAÇÃO (POST)
