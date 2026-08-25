a4a104b47fd84865f581546aaf2900eb258cc773
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-08-25 17:24:58 -0300

    fix(api): Correção no require de middleware de paginação

--- API_agrofamilia_pesca/modules/TipoProduto/routes/tipos-produtos.associacao.routes.js
+++ API_agrofamilia_pesca/modules/TipoProduto/routes/tipos-produtos.associacao.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const pagination = require("../../../middleware/pagination");
 const TipoProdutoController = require("../tipos-produtos.controller");
 
 /**
