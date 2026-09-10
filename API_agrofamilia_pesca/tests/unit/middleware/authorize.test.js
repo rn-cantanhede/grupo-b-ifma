56858061c9c993cb2d8b38436a2f840d7a87b76d
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-09-10 14:23:09 -0300

    test(api): Criação dos testes do middleware Authorize

---
+++ API_agrofamilia_pesca/tests/unit/middleware/authorize.test.js
@@ -0,0 +1,91 @@
+const Authorize = require("../../../middleware/Authorize");
+
+describe("Authorize", () => {
+    let req;
+    let res;
+    let next;
+
+    beforeEach(() => {
+        req = {
+            user: {
+                id: 1,
+                login: "admin",
+                nivel: 1,
+            },
+
+            log: {
+                warn: jest.fn(),
+                info: jest.fn(),
+                error: jest.fn(),
+            },
+        };
+
+        res = {};
+        next = jest.fn();
+    });
+
+    /*
+     * ==========================================================
+     * LEVEL
+     * ==========================================================
+     *
+     */
+    test("deve permitir usuário com nível suficiente", () => {
+        const middleware = Authorize(1);
+
+        middleware(req, res, next);
+
+        expect(next).toHaveBeenCalledWith();
+        expect(req.log.info).toHaveBeenCalledWith(
+            {
+                event: "AUTHORIZATION",
+                resource: "authorization",
+                action: "login",
+                usuarioId: 1,
+            },
+            "Usuário autorizado"
+        );
+    });
+
+    /*
+     * ==========================================================
+     * LOW LEVEL
+     * ==========================================================
+     *
+     */
+    test("deve negar acesso quando o nível do usuário for insuficiente", () => {
+        req.user.nivel = 2;
+
+        const middleware = Authorize(1);
+
+        middleware(req, res, next);
+
+        expect(next).toHaveBeenCalledWith(
+            expect.objectContaining({
+                message: "Sem permissão",
+                statusCode: 403,
+            })
+        );
+    });
+
+    /*
+     * ==========================================================
+     * UNDERFINED
+     * ==========================================================
+     *
+     */
+    test("deve negar acesso quando o nível do usuário for insuficiente", () => {
+        req.user.nivel = undefined;
+
+        const middleware = Authorize(1);
+
+        middleware(req, res, next);
+
+        expect(next).toHaveBeenCalledWith(
+            expect.objectContaining({
+                message: "Usuário não autenticado",
+                statusCode: 401,
+            })
+        );
+    });
+});
