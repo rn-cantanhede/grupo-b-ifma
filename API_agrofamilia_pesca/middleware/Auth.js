71974ef3694a6d4f14025eb1f01402c68b8328e2
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-09-12 18:32:41 -0300

    fix(api): resolvido as inconsistências na geração do token
    Bloqueia tokens gerados antes de alterações de credencias

--- API_agrofamilia_pesca/middleware/Auth.js
+++ API_agrofamilia_pesca/middleware/Auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const Erros = require("../shared/errors/Errors");
+const { findBy } = require("../shared/Utils/dbUtils");
 const secret = process.env.JWT_SECRET;
 
 /**
@@ -7,8 +8,8 @@ const secret = process.env.JWT_SECRET;
  * Este middleware será utilizado para proteger rotas
  * que exigem que o usuário esteja autenticado.
  */
-module.exports = function auth(req, res, next) {
-    const authToken = req.headers["authorization"];
+module.exports = async function auth(req, res, next) {
+    let authToken = req.headers["authorization"];
     const secret = process.env.JWT_SECRET;
 
     if (!authToken) {
@@ -27,6 +28,29 @@ module.exports = function auth(req, res, next) {
     try {
         const decoded = jwt.verify(token, secret);
         req.user = decoded;
+        const user = await findBy("ID", req.user.id, false, "usuario", 1, 1);
+
+        if (!user) {
+            return next(new Erros("Usuário não encontrado", 404));
+        };
+
+        if (user.result.NIVEL != req.user.nivel || 
+            user.result.ID_PESSOA != req.user.id || 
+            user.result.LOGIN != req.user.login || 
+            user.result.ID_SECRETARIA != req.user.secretaria ||
+            user.result.ID_ASSOCIACAO != req.user.associacao
+        ) {
+            req.log.warn({
+                event: "AUTH_TOKEN_WARN",
+                resource: "authentication",
+                action: "login"
+            }, "Inconsistência na geração do token");
+
+            return next(new Erros(
+                "Inconsistência na geração do token. Realize login novamente", 
+                400
+            ));
+        };
 
         req.log.info({
             event: "AUTH_TOKEN",
