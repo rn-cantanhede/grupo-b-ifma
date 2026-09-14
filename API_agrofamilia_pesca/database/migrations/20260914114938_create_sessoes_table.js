a0fa70919208ffdcaa357e1b9716d0ecb6e7d0f6
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-09-14 20:13:12 -0300

    feat(db): criação do migrate da sessão para gerenciamento de tokens

---
+++ API_agrofamilia_pesca/database/migrations/20260914114938_create_sessoes_table.js
@@ -0,0 +1,49 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable("SESSEOS", (table) => {
+        table.increments("ID_SESSAO").primary();
+        table.integer("ID_PESSOA").unsigned().notNullable();
+        table.integer("ID_SECRETARIA").unsigned();
+        table.integer("ID_ASSOCIACAO").unsigned();
+
+        table.string("REFRESH_TOKEN_HASH").notNullable();
+        table.dateTime("CRIADO").notNullable();
+        table.dateTime("USADO");
+        table.dateTime("EXPIRA").notNullable();
+        table.dateTime("REVOGADO");
+        table.string("IP");
+
+        table
+            .foreign("ID_PESSOA")
+            .references("ID")
+            .inTable("PESSOA")
+            .onUpdate("CASCADE")
+            .onDelete("RESTRICT");
+
+        table
+            .foreign("ID_SECRETARIA")
+            .references("ID")
+            .inTable("SECRETARIA")
+            .onUpdate("CASCADE")
+            .onDelete("RESTRICT");
+
+        table
+            .foreign("ID_ASSOCIACAO")
+            .references("ID")
+            .inTable("ASSOCIACAO")
+            .onUpdate("CASCADE")
+            .onDelete("RESTRICT");
+
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable("SESSEOS");
+};
