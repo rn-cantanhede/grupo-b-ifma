266dc18e60d9e836b890837779eb2864b02c5954
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-09-18 22:27:15 -0300

    refactor(api): Refatorado para aceitar novo padrão, criação de novas verificações e atualização das colunas REVOGADO e USADO. Sistema de geração de token atualizado para gerar automaticamente enquanto o refresh token for valido

--- API_agrofamilia_pesca/middleware/Auth.js
+++ API_agrofamilia_pesca/middleware/Auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const Erros = require("../shared/errors/Errors");
-const { findBy } = require("../shared/Utils/dbUtils");
-const secret = process.env.JWT_SECRET;
+const { findBy, updateData } = require("../shared/Utils/dbUtils");
+const authToken = require("../shared/Utils/authToken");
 
 /**
  * Exporta um middleware de autenticação.
@@ -9,10 +9,11 @@ const secret = process.env.JWT_SECRET;
  * que exigem que o usuário esteja autenticado.
  */
 module.exports = async function auth(req, res, next) {
-    let authToken = req.headers["authorization"];
+    const token = req.session.token;
+    const refreshToken = req.session.refreshToken;
     const secret = process.env.JWT_SECRET;
 
-    if (!authToken) {
+    if (!token) {
         req.log.warn({
             event: "AUTH_TOKEN_WARN",
             resource: "authentication",
@@ -22,52 +23,154 @@ module.exports = async function auth(req, res, next) {
         return next(new Erros("Token não informado", 401));
     };
 
-    const bearer = authToken.split(" ");
-    const token = bearer[1];
+    // const bearer = authToken.split(" ");
+    // const token = bearer[1];
 
     try {
         const decoded = jwt.verify(token, secret);
-        req.user = decoded;
-        const user = await findBy("ID", req.user.id, false, "usuario", 1, 1);
+        const sessao = await findBy("ID_PESSOA", decoded.id, false, "sessoes", 1, 1);
+
+        if (!sessao) {
+            await updateData(req.session.user.id, { REVOGADO: new Date() }, "sessoes");
+
+            req.log.error({
+                event: "AUTH_TOKEN_WARN",
+                resource: "authentication",
+                action: "auth"
+            }, "Sessão não encontrada");
+
+            return next(new Erros("Sessão não encontrada", 404));
+        };
+
+        if (sessao.result.EXPIRA < new Date()) {
+            req.log.error({
+                event: "AUTH_TOKEN_WARN",
+                resource: "authentication",
+                action: "auth"
+            }, "Sessão expirada");
+
+            return next(new Erros("Sessão expirada", 400));
+        };
+
+        if (sessao.result.REVOGADO != null) {
+            req.log.error({
+                event: "AUTH_TOKEN_WARN",
+                resource: "authentication",
+                action: "auth"
+            }, "Sessão revogada");
+
+            return next(new Erros("Sessão revogada", 400));
+        };
+
+        const user = await findBy("ID_PESSOA", sessao.result.ID_PESSOA, false, "usuario", 1, 1);
+
+        req.session.user = {
+            id: user.result.ID_PESSOA,
+            nivel: user.result.NIVEL,
+            login: user.result.LOGIN,
+            secretaria: user.result.ID_SECRETARIA,
+            associacao: user.result.ID_ASSOCIACAO
+        };
 
         if (!user) {
             return next(new Erros("Usuário não encontrado", 404));
         };
 
-        if (user.result.NIVEL != req.user.nivel || 
-            user.result.ID_PESSOA != req.user.id || 
-            user.result.LOGIN != req.user.login || 
-            user.result.ID_SECRETARIA != req.user.secretaria ||
-            user.result.ID_ASSOCIACAO != req.user.associacao
+        if (user.result.NIVEL != req.session.user.nivel ||
+            user.result.ID_PESSOA != req.session.user.id ||
+            user.result.LOGIN != req.session.user.login ||
+            user.result.ID_SECRETARIA != req.session.user.secretaria ||
+            user.result.ID_ASSOCIACAO != req.session.user.associacao
         ) {
+            //Revoga sessão caso tenha inconsistencia
+            await updateData(req.session.user.id, { REVOGADO: new Date() }, "sessoes");
+
             req.log.warn({
                 event: "AUTH_TOKEN_WARN",
                 resource: "authentication",
-                action: "login"
+                action: "auth"
             }, "Inconsistência na geração do token");
 
             return next(new Erros(
-                "Inconsistência na geração do token. Realize login novamente", 
+                "Inconsistência na geração do token. Realize login novamente",
                 400
             ));
         };
 
+        //Atualiza ultimo uso
+        await updateData(req.session.user.id, { USADO: new Date() }, "sessoes");
+
         req.log.info({
             event: "AUTH_TOKEN",
             resource: "authentication",
-            action: "login",
-            usuarioId: req.user.id,
+            action: "auth",
+            usuarioId: req.session.user.id,
         }, "Token válido");
 
         return next();
     } catch (error) {
-        req.log.error({
-            event: "AUTH_TOKEN_ERROR",
-            resource: "authentication",
-            action: "login"
-        }, "Token inválido ou expirado");
+        if (error.name === "TokenExpiredError") {
+            console.log("TOKEN EXPIROU!");
+
+            req.log.info({
+                event: "AUTH_TOKEN",
+                resource: "authentication",
+                action: "auth",
+                usuarioId: req.session.user.id,
+            }, "Token expirou");
+
+            const decoded = jwt.decode(token);
+
+            if (!decoded.id) {
+                req.log.error({
+                    event: "AUTH_TOKEN",
+                    resource: "authentication",
+                    action: "auth",
+                    usuarioId: req.session.user.id,
+                }, "Token inválido");
+
+                return next(new Erros("Token inválido", 401));
+            };
+
+            const sessao = await findBy(
+                "ID_PESSOA",
+                decoded.id,
+                false,
+                "sessoes",
+                1,
+                1
+            );
+
+            if (!sessao) {
+                req.log.error({
+                    event: "AUTH_TOKEN",
+                    resource: "authentication",
+                    action: "auth",
+                    usuarioId: req.session.user.id,
+                }, "Sessão não encontrada");
+
+                return next(new Erros("Sessão não encontrada", 404));
+            };
+
+            const newToken = await authToken.createToken(
+                sessao.result,
+                refreshToken
+            );
+
+            req.session.token = newToken.token;
+
+            req.log.info({
+                event: "AUTH_TOKEN",
+                resource: "authentication",
+                action: "auth",
+                usuarioId: req.session.user.id,
+            }, "Token gerado");
+
+            return next();
+        };
 
         console.log(error);
-        return next(new Erros("Token inválido ou expirado", 401));
+
+        return next(new Erros("Token inválido", 401));
     };
 };
