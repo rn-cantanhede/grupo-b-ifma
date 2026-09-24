73cb477642b4f81baff8e1203c07a44ebc60570b
rn-cantanhede <renan.cantanhede.cardozo@gmail.com>
2026-09-24 10:07:33 -0300

    refactor(api): Refatoção nos parametros e implementação do hateoas nos find do controller de Categorias

--- API_agrofamilia_pesca/modules/Categorias/categorias.controller.js
+++ API_agrofamilia_pesca/modules/Categorias/categorias.controller.js
@@ -1,3 +1,5 @@
+const { convertString } = require("../../shared/Utils/findUtils");
+const Hateoas = require("../../shared/Utils/hateoas");
 const CategoriasService = require("./categorias.service");
 
 /**
@@ -14,25 +16,39 @@ class CategoriasController {
     async AllCategorias(req, res) {
         try {
             const categorias = await CategoriasService.findAllCategorias(
-                req.user,
+                req.session.user,
                 req.query.page,
                 req.query.limit
+            );
+            const hateoas = Hateoas(
+                categorias.result[0].ID,
+                process.env.URL,
+                req.session.user.nivel,
+                "categorias",
+                ["",
+                    categorias.result[0].ID,
+                    convertString(categorias.result[0].NOME)
+                ]
             );
 
             req.log.info({
                 event: "CATEGORIA_LIST",
                 resource: "categoria",
                 action: "list",
-                usuarioID: req.user.id
+                usuarioID: req.session.user.id
             }, "Listagem das categorias");
 
-            return res.status(200).json(categorias);
+            return res.status(200).json({
+                result: categorias.result,
+                total: categorias.total,
+                hateoas: hateoas
+            });
         } catch (error) {
             req.log.error({
                 event: "CATEGORIA_LIST_ERROR",
                 resource: "categoria",
                 action: "list",
-                usuarioID: req.user.id
+                usuarioID: req.session.user.id
             }, "Erro ao listar as categorias");
 
             console.log(error);
@@ -48,26 +64,55 @@ class CategoriasController {
         try {
             const result = await CategoriasService.find(
                 req.params.value,
-                req.user,
+                req.session.user,
                 req.query.page,
                 req.query.limit
             );
+            let hateoas;
+
+            if (!Array.isArray(result.result)) {
+                hateoas = Hateoas(
+                    result.result.ID,
+                    process.env.URL,
+                    req.session.user.nivel,
+                    "categorias",
+                    ["",
+                        result.result.ID,
+                        convertString(result.result.NOME)
+                    ]
+                );
+            } else {
+                hateoas = Hateoas(
+                    result.result[0].ID,
+                    process.env.URL,
+                    req.session.user.nivel,
+                    "categorias",
+                    ["",
+                        result.result[0].ID,
+                        convertString(result.result[0].NOME)
+                    ]
+                );
+            };
 
             req.log.info({
                 event: "CATEGORIA_FIND",
                 resource: "categoria",
                 action: "find",
-                usuarioId: req.user.id,
+                usuarioId: req.session.user.id,
                 target: req.params.value
             }, "Categoria consultada por id ou nome");
 
-            return res.status(200).json(result);
+            return res.status(200).json({
+                result: result.result,
+                total: result.total,
+                hateoas: hateoas
+            });
         } catch (error) {
             req.log.error({
                 event: "CATEGORIA_FIND_ERROR",
                 resource: "categoria",
                 action: "find",
-                usuarioID: req.user.id,
+                usuarioID: req.session.user.id,
                 target: req.params.value
             }, "Erro ao buscar categoria");
 
@@ -84,23 +129,42 @@ class CategoriasController {
         try {
             const result = await CategoriasService.createCategoria(
                 req.body,
-                req.user
+                req.session.user
+            );
+            const find = await CategoriasService.find(
+                convertString(result.NOME),
+                req.session.user,
+                1,
+                1
+            );
+            const hateoas = Hateoas(
+                find.result[0].ID,
+                process.env.URL,
+                req.session.user.nivel,
+                "categorias",
+                ["",
+                    find.result[0].ID,
+                    convertString(find.result[0].NOME)
+                ]
             );
 
             req.log.info({
                 event: "CATEGORIA_CREATE",
                 resource: "categoria",
                 action: "create",
-                usuarioId: req.user.id,
+                usuarioId: req.session.user.id,
             }, "Categoria criada");
 
-            return res.status(201).json(result);
+            return res.status(201).json({
+                result: result,
+                hateoas: hateoas
+            });
         } catch (error) {
             req.log.error({
                 event: "CATEGORIA_CREATE_ERROR",
                 resource: "categoria",
                 action: "create",
-                usuarioId: req.user.id,
+                usuarioId: req.session.user.id,
             }, "Erro ao criar categoria");
 
             console.log(error);
@@ -117,24 +181,43 @@ class CategoriasController {
             const result = await CategoriasService.updateCategoria(
                 req.params.id,
                 req.body,
-                req.user
+                req.session.user
+            );
+            const find = await CategoriasService.find(
+                req.params.id,
+                req.session.user,
+                1,
+                1
+            );
+            const hateoas = Hateoas(
+                find.result.ID,
+                process.env.URL,
+                req.session.user.nivel,
+                "categorias",
+                ["",
+                    find.result.ID,
+                    convertString(find.result.NOME)
+                ]
             );
 
             req.log.info({
                 event: "CATEGORIA_UPDATE",
                 resource: "categoria",
                 action: "update",
-                usuarioId: req.user.id,
+                usuarioId: req.session.user.id,
                 targetId: req.params.id
             }, "Categoria atualizada");
 
-            return res.status(200).json(result);
+            return res.status(200).json({
+                result: result,
+                hateoas: hateoas
+            });
         } catch (error) {
             req.log.error({
                 event: "CATEGORIA_UPDATE_ERROR",
                 resource: "categoria",
                 action: "update",
-                usuarioId: req.user.id,
+                usuarioId: req.session.user.id,
                 targetId: req.params.id
             }, "Erro ao atualizar categoria");
 
@@ -151,24 +234,39 @@ class CategoriasController {
         try {
             const result = await CategoriasService.deleteCategoria(
                 req.params.id,
-                req.user
+                req.session.user
+            );
+
+            const hateoas = Hateoas(
+                "",
+                process.env.URL,
+                req.session.user.nivel,
+                "categorias",
+                [""]
             );
 
             req.log.info({
                 event: "CATEGORIA_DELETE",
                 resource: "categoria",
                 action: "delete",
-                usuarioId: req.user.id,
+                usuarioId: req.session.user.id,
                 targetId: req.params.id
             }, "Categoria excluída");
 
-            return res.status(200).json(result);
+            return res.status(200).json({
+                result: result.result,
+                total: result.total,
+                hateoas: {
+                    GET: hateoas.GET,
+                    POST: hateoas.POST
+                }
+            });
         } catch (error) {
             req.log.error({
                 event: "CATEGORIA_DELETE_ERROR",
                 resource: "categoria",
                 action: "delete",
-                usuarioId: req.user.id,
+                usuarioId: req.session.user.id,
                 targetId: req.params.id
             }, "Erro ao apagar categoria");
 
